48e94c763763500ed9d054f489f972561b6ac93b
Cazares <chaotears@gmail.com>
2021-06-10 22:10:02 -0600

    Base app with tailwind done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Quicksand': ['Quicksand', 'sans-serif'],
+      },
+      animation: {
+        'pulse-slow': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
   },
   variants: {
     extend: {},
