e9c23297985afd3c563ece0a7c7ce4ed02c1bec5
Cazares <chaotears@gmail.com>
2021-11-03 23:23:16 -0600

    Change to animation on Info cards when changing from dark to ligth mode

--- components/about/Tecnologie.jsx
+++ components/about/Tecnologie.jsx
@@ -23,7 +23,7 @@ const tecnologie = ({name, svgname, idtext}) => {
         <>
                 <div className="flex flex-col items-center justify-center my-2">
                     <img className="m-3 h-[80px] w-[80px] md:h-[110px] md:w-[110px] lg:h-[140px] lg:w-[140px]" src={`/tecnologies/${svgname}.svg`} alt="Technologie" loading="lazy" />
-                    <p className="mx-auto dark:text-white text-black font-bold text-base lg:text-xl popovers">{name}</p>
+                    <p className="mx-auto dark:text-white text-black font-bold text-base lg:text-xl popovers duration-300 ease-in">{name}</p>
                 </div>
         {/* <Button onClick={handleClick}>
                 <div className="flex flex-col items-center justify-center">
