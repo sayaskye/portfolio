8f3b9d571b44d30d4782408042fb9d230e84a833
Andres Cazares <chaotears@gmail.com>
2022-11-16 03:58:19 -0600

    refactor: portfolio done

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -6,134 +6,72 @@ import HeaderSeo from "../components/seo/HeaderSeo";
 import Layout from "../components/layout";
 import PortfolioCard from "../components/portfolio/PortfolioCard";
 
+import {
+  portfolioBack,
+  portfolioCourses,
+  portfolioFront,
+} from "../components/portfolio/projects";
 import { configContext } from "../context/configContext";
 
 const Portfolio = () => {
   const { locale } = useContext(configContext);
+
   return (
     <>
       <HeaderSeo locale={locale} page="portfolio" />
       <Layout>
         <div className="w-screen h-auto pb-[54px] bg-gray-200 duration-500 ease-in dark:bg-gray-800">
-          <div className="container mx-auto">
-            <div className=" mx-auto lg:mx-20  my-10 dark:text-white duration-300 ease-in">
-              <h1 className="text-main-lightblue dark:text-main-blue text-5xl my-10 font-bold mx-3 duration-300 ease-in text-center">
-                <FormattedMessage
-                  id="portfolio.title"
-                  defaultMessage="Projects"
-                />
-              </h1>
-              <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in ">
-                <FormattedMessage id="portfolio.front" defaultMessage="" />
-              </h3>
-              <div className=" grid grid-cols-1 xl:grid-cols-2 ">
+          <h1 className="text-main-lightblue dark:text-main-blue text-5xl my-10 font-bold mx-3 duration-300 ease-in text-center">
+            <FormattedMessage id="portfolio.title" />
+          </h1>
+          <div className="container mx-auto my-10 dark:text-white duration-300 ease-in">
+            <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in ">
+              <FormattedMessage id="portfolio.front" />
+            </h3>
+            <div className=" grid grid-cols-1 xl:grid-cols-2 ">
+              {portfolioFront.map((item, index) => (
                 <PortfolioCard
-                  title="portfolio"
-                  imageType="portfolio"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={false}
-                  url=""
+                  key={index}
+                  title={item.title}
+                  imageType={item.imageType}
+                  videoId={item.videoId}
+                  videoBool={item.videoBool}
+                  urlBool={item.urlBool}
+                  url={item.url}
                 />
+              ))}
+            </div>
+            <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in mt-10">
+              <FormattedMessage id="portfolio.back" />
+            </h3>
+            <div className=" grid grid-cols-1 xl:grid-cols-2 ">
+              {portfolioBack.map((item, index) => (
                 <PortfolioCard
-                  title="personalBlog"
-                  imageType="personalblog"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={true}
-                  url="https://www.blog.andrescazares.com/"
+                  key={index}
+                  title={item.title}
+                  imageType={item.imageType}
+                  videoId={item.videoId}
+                  videoBool={item.videoBool}
+                  urlBool={item.urlBool}
+                  url={item.url}
                 />
+              ))}
+            </div>
+            <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in mt-10">
+              <FormattedMessage id="portfolio.course" />
+            </h3>
+            <div className=" grid grid-cols-1 xl:grid-cols-2 ">
+              {portfolioCourses.map((item, index) => (
                 <PortfolioCard
-                  title="ASMPC"
-                  imageType="asmpc"
-                  videoId="lWhj9hW31Js"
-                  videoBool={true}
-                  urlBool={false}
-                  url=""
+                  key={index}
+                  title={item.title}
+                  imageType={item.imageType}
+                  videoId={item.videoId}
+                  videoBool={item.videoBool}
+                  urlBool={item.urlBool}
+                  url={item.url}
                 />
-                <PortfolioCard
-                  title="CDC"
-                  imageType="cdc"
-                  videoId="x4eM3QJlwDE"
-                  videoBool={true}
-                  urlBool={false}
-                  url=""
-                />
-                <PortfolioCard
-                  title="weather"
-                  imageType="weather"
-                  videoId="0gCM5yIAZ7Q"
-                  videoBool={true}
-                  urlBool={true}
-                  url="https://www.clima.andrescazares.com"
-                />
-                <PortfolioCard
-                  title="taquero"
-                  imageType="videogame"
-                  videoId="g-3pOXJKiiE"
-                  videoBool={true}
-                  urlBool={false}
-                  url=""
-                />
-              </div>
-              <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in mt-10">
-                <FormattedMessage id="portfolio.back" defaultMessage="" />
-              </h3>
-              <div className=" grid grid-cols-1 xl:grid-cols-2 ">
-                <PortfolioCard
-                  title="strapiBlog"
-                  imageType="strapi"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={true}
-                  url="https://github.com/sayaskye/blog-strapi"
-                />
-              </div>
-              <h3 className="text-main-lightblue dark:text-main-blue text-xl font-bold mx-5 lg:mx-3 duration-300 ease-in mt-10">
-                <FormattedMessage id="portfolio.course" defaultMessage="" />
-              </h3>
-              <div className=" grid grid-cols-1 xl:grid-cols-2 ">
-                <PortfolioCard
-                  title="ProductHunt"
-                  imageType="web"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={true}
-                  url="https://www.phc.andrescazares.com"
-                />
-                <PortfolioCard
-                  title="chat"
-                  imageType="chat"
-                  videoId="D3u3pPRJStk"
-                  videoBool={true}
-                  urlBool={true}
-                  url="https://www.superchat.andrescazares.com"
-                />
-                <PortfolioCard
-                  title="blogphp"
-                  imageType="phpblog"
-                  videoId="dpMQ3WPNOqE"
-                  videoBool={true}
-                  urlBool={false}
-                  url=""
-                />
-                <PortfolioCard
-                  title="apiLaravel"
-                  imageType="apis"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={true}
-                  url="https://github.com/sayaskye/apirestlaravel"
-                />
-                <PortfolioCard
-                  title="apiNode"
-                  imageType="apis"
-                  videoId=""
-                  videoBool={false}
-                  urlBool={true}
-                  url="https://github.com/sayaskye/apirestnodejs"
-                />
-              </div>
+              ))}
             </div>
           </div>
         </div>
